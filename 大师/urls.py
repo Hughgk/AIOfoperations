9dc59724dbbb20dac68612863e35af6b7f00a484
Hughgk <991325480@qq.com>
2019-12-05 22:56:10 +0800

    Add files via upload

--- 大师/urls.py
+++ 大师/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.main_page, name="main_page"),
+    url(r'^main_page', views.main_page, name="main_page"),
+    url(r'^$', views.index, name="index"),
     url(r'^get_result', views.get_result, name="get_result"),
+    url(r'^login', views.login, name="login"),
+    url(r'^register', views.register, name="register"),
 ]
